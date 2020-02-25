f7486799d9f9b58dc85bffbbf663294b534b9d25
Vick <vlusionwebbuilding@gmail.com>
2020-02-25 18:56:25 +0100

    Back to index knop weg

---
+++ includes/voorstelling.php
@@ -0,0 +1,41 @@
+<?php
+
+class Voorstelling {
+    public function fetch_all($isActief) {
+        global $pdo;
+
+        $query = $pdo->prepare("SELECT * FROM voorstelling where isActief = $isActief");
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+
+    public function getHtml($isActief = 0)
+    {
+        $voorstellingen = $this->fetch_all($isActief);
+        $html = "";
+        foreach ($voorstellingen as $voorstelling) {
+            $html .= "<div class=\"text-center\">
+                        <h3>{$voorstelling['voorstellingNaam']}</h3>
+                       <a href='voorstellingdetails.php?id={$voorstelling['voorstellingID']}'><img src=\"{$voorstelling['imagePath']}\"></a>
+                    </div>
+                    <a href='voorstellingen.php?id={$voorstelling['voorstellingID']}'>Verwijder</a>
+                    <a href='editvoorstelling.php?editID={$voorstelling['voorstellingID']}'>Pas aan</a>";
+
+        }
+        return $html;
+    }
+
+
+
+//    public function fetch_data($candy_id) {
+//        global $pdo;
+//
+//        $query = $pdo->prepare("SELECT * FROM team WHERE candy_id = ?");
+//        $query->bindValue(1, $candy_id);
+//        $query->execute();
+//
+//        return $query->fetch();
+//    }
+}
+?>
---
+++ includes/voorstellingdatum.php
@@ -0,0 +1,47 @@
+<?php
+
+class Voorstellingdatum {
+    public function fetch_all($voorstellingID) {
+        global $pdo;
+
+        $query = $pdo->prepare("SELECT * FROM voorstellingdatums where voorstellingID = $voorstellingID");
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+
+    public function getVoorstellingDates($voorstellingID)
+    {
+        $voorstellingDatums = $this->fetch_all($voorstellingID);
+        $html = "";
+
+        $count = 0;
+        foreach ($voorstellingDatums as $datum) {
+            $timestamp = strtotime($datum["voorstellingDatum"]);
+            $jaar = date("Y", $timestamp);
+            $maand = date("M", $timestamp);
+            $dag = date("d", $timestamp);
+            $count++;
+
+            $html .= "      
+                    <li>
+                        <time datetime=\"2014-07-20\">
+                            <span class=\"day\">$dag</span>
+                            <span class=\"month\">$maand</span>
+                            <span class=\"year\">$jaar</span>
+                            <span class=\"time\">ALL DAY</span>
+                        </time>
+                        <div class=\"info\">
+                            <h2 class=\"title\">Opvoering $count</h2>
+                            <p class=\"desc\">12.00</p>
+                        </div>
+                    </li>";
+
+        }
+        return $html;
+    }
+}
+
+
+
+?>
