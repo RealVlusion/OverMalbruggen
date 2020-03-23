7142d476993af789c68e5786985c882f8c19fd8e
Vick <vlusionwebbuilding@gmail.com>
2020-03-23 17:37:38 +0100

    Succes button change

--- site/voorstellingdetails.php
+++ site/voorstellingdetails.php
@@ -105,7 +105,7 @@ $teampVoorstellingdatums = $stmt2->fetch();
             <?php
                 $voorstellingNaam = $tempVoorstelling['voorstellingNaam'];
                 if ($tempVoorstelling['isActief'] == 1) {
-                    echo "<h3 class='centerText'> $voorstellingNaam <span class='badge badge-danger'>Actief</span></h3>";
+                    echo "<h3 class='centerText'> $voorstellingNaam <span class='badge badge-success'>Actief</span></h3>";
                 }
                 else {
                 echo "<h3 class='centerText'> $voorstellingNaam <span class='badge badge-danger'>Archief</span></h3>";
