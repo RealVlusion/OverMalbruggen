35a79d5b0601768403527214d945ecb9d8e69f56
Vick <vlusionwebbuilding@gmail.com>
2020-03-24 18:55:01 +0100

    VoorstellingFix

--- includes/connection.php
+++ includes/connection.php
@@ -2,7 +2,7 @@
 
 try{
 
-    $pdo = new PDO('mysql:host=localhost;dbname=overMalbruggenDb', 'root', 'admin');
+    $pdo = new PDO('mysql:host=localhost;dbname=overMalbruggenDb', 'root', '');
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e){
     exit('Database error. - ' . $e);
