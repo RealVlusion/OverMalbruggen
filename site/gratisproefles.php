1437e9ce50a91d81540adb9752b4dd9550344e33
Vick <vlusionwebbuilding@gmail.com>
2020-04-03 14:44:55 +0200

    imgNieuws resize

--- site/gratisproefles.php
+++ site/gratisproefles.php
@@ -66,7 +66,7 @@
     <div class="jumbotron jumbotron-fluid text-center">
         <div class="container">
             <h1 class="display-4">OverMalbruggen</h1>
-            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            <p class="lead">Gratis proefles.</p>
         </div>
     </div>
 </header>
